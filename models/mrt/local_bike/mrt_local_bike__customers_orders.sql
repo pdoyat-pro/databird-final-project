deeff0bf2f2d4abde296edafc80c3b6d96ba0c76
Pauline Doyat <pdoyat@groupeseb.com>
2025-02-04 09:12:16 +0000

    Adding mart orders and products

--- models/mrt/local_bike/mrt_local_bike__customers_orders.sql
+++ models/mrt/local_bike/mrt_local_bike__customers_orders.sql
@@ -2,6 +2,8 @@ SELECT customer_id,
     city,
     state,
     COUNT(order_id) as nb_orders,
+    SUM(total_order_amount) as total_sales_amount,
+    SUM(total_quantity) AS total_sales_volume,
     AVG(total_order_amount) as avg_shopping_basket,
     SUM(total_order_amount)/SUM(total_quantity) as avg_item_price,
 FROM {{ ref('int_local_bike__orders' )}} 
